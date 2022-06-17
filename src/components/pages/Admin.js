4d52e75330189e559dc8eb5b1041107e1e34ad89
coding-to-music <connors.tom@gmail.com>
2022-06-17 20:37:38 +0000

    Build

--- src/components/pages/Admin.js
+++ src/components/pages/Admin.js
@@ -24,7 +24,7 @@ function Admin() {
     >
       <Loading />
     </Container>
-  ) : document?.type === "admin" && documents.length >= 0 ? (
+  ) : document?.type === "admin" && documents.length > 0 ? (
     <Container className="mx-auto justify-content-center">
       <Row className=" d-flex justify-content-center ">
         <Col md="auto" className="d-flex justify-content-center ">
